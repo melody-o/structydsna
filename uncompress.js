f477c5d8627aa9be14cc2625fcd249cc2b25440e
Justin <burgoniojustin@yahoo.com>
2023-11-10 13:14:22 -0800

    compress, uncompress

---
+++ uncompress.js
@@ -0,0 +1,29 @@
+Write a function, uncompress, that takes in a string as an argument. The input string will be formatted into multiple groups according to the following pattern:
+
+<number><char>
+
+for example, '2c' or '3a'.
+The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively. You may assume that the input string is well-formed according to the previously mentioned pattern.
+
+
+const uncompress = (s) => {
+  // todo
+  let str = []
+  let num = '0123456789'
+  let i = 0
+  let j = 0
+  
+  while (j < s.length){
+    if(num.includes(s[j])){
+      j+=1
+    } else{
+      let val = Number(s.slice(i,j));
+      for (let k = 0; k < val; k++){
+        str.push(s[j])
+      }
+      j+=1
+      i = j
+    }
+  }
+  return str.join('')
+};
