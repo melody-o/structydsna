a0fd05d82eacc9fc1abeed5439ae23fd649eadaf
Melody <melodyoberg@icloud.com>
2023-11-15 14:51:25 -0800

    pairproduct

--- javascript/pairProduct.js
+++ javascript/pairProduct.js
@@ -1,3 +1,12 @@
+// pair product
+// Write a function, pairProduct, that takes in an array and a target product as arguments. The function should return an array containing a pair of indices whose elements multiply to the given target. The indices returned must be unique.
+
+// Be sure to return the indices, not the elements themselves.
+
+// There is guaranteed to be one such pair whose product is the target.
+
+
+
 const pairProduct = (numbers, targetProduct) => {
     // todo
     let indxObj = {}
@@ -17,4 +26,21 @@ const pairProduct = (numbers, targetProduct) => {
     indxArr.push(i)
     // console.log(indxArr)
     return indxArr
+  };
+
+//ideal way
+  const pairProduct = (numbers, targetProduct) => {
+    // todo
+    let indxObj = {}
+
+
+    for (let i = 0; i < numbers.length; i++){
+      console.log(i)
+      let val = numbers[i]
+      let prod = targetProduct / val
+      if (prod in indxObj){
+        return [i, indxObj[prod]]
+      }
+      indxObj[val] = i
+    }
   };
--- python/pair_sum.py
+++ python/pair_sum.py
@@ -1,3 +1,12 @@
+# pair sum
+# Write a function, pairSum, that takes in an array and a target sum as arguments. The function should return an array containing a pair of indices whose elements sum to the given target. The indices returned must be unique.
+
+# Be sure to return the indices, not the elements themselves.
+
+# There is guaranteed to be one such pair that sums to the target.
+
+
+
 def pair_sum(numbers, target_sum):
   indx = []
   for num in range(len(numbers)):
