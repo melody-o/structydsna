7b18b8f4896617a660107870f4ad0496f0653d5e
Melody <melodyoberg@icloud.com>
2023-11-15 14:12:02 -0800

    pair sum.py

--- python/pair_sum.py
+++ python/pair_sum.py
@@ -8,3 +8,14 @@ def pair_sum(numbers, target_sum):
         indx.append(num)
         indx.append(number)
   return tuple(indx)
+
+#ideally use a hashmap
+def pair_sum(numbers, target_sum):
+  indx = {}
+
+  for index, num in enumerate(numbers):
+    tple = target_sum - num
+
+    if tple in indx:
+      return (indx[tple], index)
+    indx[num] = index
