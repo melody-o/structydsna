290b1bb6f7296bb4da102c119e7eacedff78d67b
Melody <melodyoberg@icloud.com>
2023-12-27 10:50:47 -0800

    problem

--- javascript/intersection.js
+++ javascript/intersection.js
@@ -1,4 +1,7 @@
+// intersection
+// Write a function, intersection, that takes in two arrays, a,b, as arguments. The function should return a new array containing elements that are in both of the two arrays.
 
+// You may assume that each input array does not contain duplicate elements.
 
 
 
