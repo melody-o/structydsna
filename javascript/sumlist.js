1a4b29ee037e74fd4a9e493c75d025b3fc2e1e0d
Melody <melodyoberg@icloud.com>
2024-01-09 13:53:05 -0800

    sumlist solution

--- javascript/sumlist.js
+++ javascript/sumlist.js
@@ -1,1 +1,15 @@
 // Write a function, sumList, that takes in the head of a linked list containing numbers as an argument. The function should return the total sum of all values in the linked list.
+
+const sumList = (head) => {
+    // todo
+    // let sum = 0;
+    // let current = head;
+    // while (current !== null) {
+    //   sum += current.val
+    //   current = current.next
+    // }
+    // return sum
+
+    if (head === null) return 0;
+    return head.val + sumList(head.next);
+  };
