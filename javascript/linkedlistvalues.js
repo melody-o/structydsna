c6c6f6c9366c2ad0f28ee03e5561e782c3c7f035
Melody <melodyoberg@icloud.com>
2024-01-09 13:28:52 -0800

    ll question

---
+++ javascript/linkedlistvalues.js
@@ -0,0 +1,2 @@
+// linked list values
+// Write a function, linkedListValues, that takes in the head of a linked list as an argument. The function should return an array containing all values of the nodes in the linked list.
