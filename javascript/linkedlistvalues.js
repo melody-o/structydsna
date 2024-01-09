26015204425308038e53b32ae2f6e0e9bc2fb2d0
Melody <melodyoberg@icloud.com>
2024-01-09 13:29:20 -0800

    ll solution

--- javascript/linkedlistvalues.js
+++ javascript/linkedlistvalues.js
@@ -1,2 +1,23 @@
 // linked list values
 // Write a function, linkedListValues, that takes in the head of a linked list as an argument. The function should return an array containing all values of the nodes in the linked list.
+
+const linkedListValues = (head) => {
+    // todo
+    let arr = [];
+    // let current = head;
+
+    // while (current !== null) {
+    //   arr.push(current.val)
+    //   current = current.next
+    // }
+
+    fillVal(head, arr)
+
+    return arr
+  };
+
+  const fillVal = (head, arr) => {
+    if (head === null) return;
+    arr.push(head.val)
+    fillVal(head.next, arr)
+  }
