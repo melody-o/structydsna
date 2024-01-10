641a047951fb657e184bfc138199a88e44d9c09c
Melody <melodyoberg@icloud.com>
2024-01-10 11:39:44 -0800

    solution

--- javascript/betweenTwoSets.js
+++ javascript/betweenTwoSets.js
@@ -22,3 +22,35 @@
 // Returns
 
 // int: the number of integers that are between the sets
+
+function getTotalX(a, b) {
+    //iterate through our array
+    //check for the max in the first array
+    //check for min in second
+    //for loops for min
+    //check if the min can be modulod by each elements of both arrays, if yes count++
+    //return count
+    let count = 0;
+    let max = -Infinity;
+    let min = Infinity;
+
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        for (let j = 0; j < b.length; j++) {
+            if (b[j] < min) {
+             min = b[j];
+            }
+        }
+        for (let k = max; k <= min; k++) {
+            if (a.every(ele => k % ele === 0)){
+                if (b.every(ele => ele % k === 0)) {
+                    count++;
+                }
+            }
+        }
+    return count;
+
+    }
